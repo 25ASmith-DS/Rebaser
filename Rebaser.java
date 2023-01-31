80c3bf7ed28647a563acddcc4334631f436b115d
A Smith <ajs.mail.home@gmail.com>
2023-01-31 16:43:09 -0500

    final javadoc

--- Rebaser.java
+++ Rebaser.java
@@ -1,10 +1,16 @@
+/**
+* Converts positive integers between different number bases
+*
+* The class stores a number, which then can be accessed with the getValue, convertToBase10, and convertToBaseN methods
+*/
+
 public class Rebaser {
 
-
     static final char[] digits = {
-        '0' , '1' , '2' , '3' , '4' , '5' ,
-        '6' , '7' , '8' , '9' , 'a' , 'b' ,
-        'c' , 'd' , 'e' , 'f'
+        '0', '1', '2', '3',
+        '4', '5', '6', '7',
+        '8', '9', 'A', 'B',
+        'C', 'D', 'E', 'F'
     };
     
     private String v;
